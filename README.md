6bed5bb2ed84e17278cc0e6eb902c86d244c09d2
somrat <monnaf37@gmail.com>
2019-05-13 14:19:12 +0600

    initialize template

---
+++ README.md
@@ -0,0 +1,62 @@
+<div align="center">
+
+# <a href="https://themefisher.com/" target="_blank" rel="noopener noreferrer">Kross Creative Portfolio Template (MIT License)</a>
+
+![GitHub stars](https://img.shields.io/github/stars/themefisher/kross-hugo.svg?color="brightgreen"&style=flat-square)
+![GitHub forks](https://img.shields.io/github/forks/themefisher/kross-hugo.svg?color="success"&style=flat-square)
+![GitHub issues closed](https://img.shields.io/github/issues-closed-raw/themefisher/kross-hugo.svg?color="orange"&style=flat-square)
+![license](https://img.shields.io/badge/license-MIT-blue.svg?&style=flat-square)
+
+</div>
+
+<div align="center">
+<img src="images/kross-homepage.gif"  width="100%" alt="kross portfolio template by themefisher">
+</div>
+
+**Kross Portfolio Templates** - Kross is a creative, sleek and modern portfolio website template with a gorgeous appearance. It appears with an amazing, stunning graphics. The minimal interface with the necessary sections will surely help the visitors to get a thorough idea about your works.Put together beginners in mind, kross is very easy to use and built with a clean and valid international standard coding structure.
+
+## Table of Contents
+
+- [Demo](#demo)
+- [Quick Start](#quick-start)
+- [Plugins](#plugins)
+- [Reporting Issues](#reporting-issues)
+- [Technical Support or Questions](#technical-support-or-questions)
+- [Licensing](#licensing)
+
+## Demo
+
+| Homepage  | About  | Blog  | Portfolio  | Contact  |
+|---|---|---|---|---|
+| [![Homepage](images/homepage.png)](http://demo.themefisher.com/kross/)  | [![About](images/about.png)](http://demo.themefisher.com/kross/about.html) | [![Blog](images/blog.png)](http://demo.themefisher.com/kross/blog.html) | [![portfolio](images/portfolio.png)](http://demo.themefisher.com/kross/portfolio.html) | [![contact](images/contact.png)](http://demo.themefisher.com/kross/contact.html)
+
+[Live Preview](http://demo.themefisher.com/kross).
+
+## Quick Start
+Quick start options:
+
+- Clone the repo: `git clone https://github.com/themefisher/kross-hugo.git`.
+- [Download from Github](https://github.com/themefisher/kross-hugo/archive/master.zip).
+- [Download from themefisher website](https://themefisher.com/products/kross-creative-portfolio-template).
+
+## Plugins
+* Bootstrap - http://getbootstrap.com
+* Jquery - 	http://www.chartjs.org/
+* Shuffle - 	https://vestride.github.io/Shuffle/
+* slick slider - 	https://kenwheeler.github.io/slick/
+* Themify Icons - 	https://themify.me/themify-icons
+
+
+## Reporting Issues
+
+We use GitHub Issues as the official bug tracker for the **Kross Theme**. Please Search [existing issues](https://github.com/themefisher/kross-hugo/issues). It’s possible someone has already reported the same problem.
+If your problem or idea is not addressed yet, [open a new issue](https://github.com/themefisher/kross-hugo/issues/new)
+
+## Technical Support or Questions
+
+If you have questions or need help integrating the product please [contact us](mailto:themefisher@gmail.com) instead of opening an issue.
+
+## Licensing
+
+- Copyright 2019 Themefisher (https://themefisher.com/)
+- Licensed under MIT (https://github.com/themefisher/kross-hugo/blob/master/LICENSE)
---
+++ assets/js/script.js
@@ -0,0 +1,162 @@
+(function ($) {
+    'use strict';
+
+
+    // Preloader js    
+    $(window).on('load', function () {
+        $('.preloader').fadeOut(100);
+    });
+
+
+    // Sticky Menu
+    $(window).scroll(function () {
+        if ($('.navigation').offset().top > 100) {
+            $('.navigation').addClass('nav-bg');
+        } else {
+            $('.navigation').removeClass('nav-bg');
+        }
+    });
+
+    // Background-images
+    $('[data-background]').each(function () {
+        $(this).css({
+            'background-image': 'url(' + $(this).data('background') + ')'
+        });
+    });
+
+    // background color
+    $('[data-color]').each(function () {
+        $(this).css({
+            'background-color': $(this).data('color')
+        });
+    });
+
+    // progress bar
+    $('[data-progress]').each(function () {
+        $(this).css({
+            'bottom': $(this).data('progress')
+        });
+    });
+
+
+    /* ########################################### hero parallax ############################################## */
+    window.onload = function () {
+
+        var parallaxBox = document.getElementById('parallax');
+        var
+            /* c1left = document.getElementById('l1').offsetLeft,
+            c1top = document.getElementById('l1').offsetTop, */
+            c2left = document.getElementById('l2').offsetLeft,
+            c2top = document.getElementById('l2').offsetTop,
+            c3left = document.getElementById('l3').offsetLeft,
+            c3top = document.getElementById('l3').offsetTop,
+            c4left = document.getElementById('l4').offsetLeft,
+            c4top = document.getElementById('l4').offsetTop,
+            c5left = document.getElementById('l5').offsetLeft,
+            c5top = document.getElementById('l5').offsetTop,
+            c6left = document.getElementById('l6').offsetLeft,
+            c6top = document.getElementById('l6').offsetTop,
+            c7left = document.getElementById('l7').offsetLeft,
+            c7top = document.getElementById('l7').offsetTop,
+            c8left = document.getElementById('l8').offsetLeft,
+            c8top = document.getElementById('l8').offsetTop,
+            c9left = document.getElementById('l9').offsetLeft,
+            c9top = document.getElementById('l9').offsetTop;
+
+        parallaxBox.onmousemove = function (event) {
+            event = event || window.event;
+            var x = event.clientX - parallaxBox.offsetLeft,
+                y = event.clientY - parallaxBox.offsetTop;
+
+            /*  mouseParallax('l1', c1left, c1top, x, y, 5); */
+            mouseParallax('l2', c2left, c2top, x, y, 25);
+            mouseParallax('l3', c3left, c3top, x, y, 20);
+            mouseParallax('l4', c4left, c4top, x, y, 35);
+            mouseParallax('l5', c5left, c5top, x, y, 30);
+            mouseParallax('l6', c6left, c6top, x, y, 45);
+            mouseParallax('l7', c7left, c7top, x, y, 30);
+            mouseParallax('l8', c8left, c8top, x, y, 25);
+            mouseParallax('l9', c9left, c9top, x, y, 40);
+        };
+
+    };
+
+    function mouseParallax(id, left, top, mouseX, mouseY, speed) {
+        var obj = document.getElementById(id);
+        var parentObj = obj.parentNode,
+            containerWidth = parseInt(parentObj.offsetWidth),
+            containerHeight = parseInt(parentObj.offsetHeight);
+        obj.style.left = left - (((mouseX - (parseInt(obj.offsetWidth) / 2 + left)) / containerWidth) * speed) + 'px';
+        obj.style.top = top - (((mouseY - (parseInt(obj.offsetHeight) / 2 + top)) / containerHeight) * speed) + 'px';
+    }
+    /* ########################################### /hero parallax ############################################## */
+
+    // testimonial-slider
+    $('.testimonial-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        arrows: false,
+        adaptiveHeight: true
+    });
+
+
+    // clients logo slider
+    $('.client-logo-slider').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        dots: false,
+        arrows: false,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 400,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    // Shuffle js filter and masonry
+    var Shuffle = window.Shuffle;
+    var jQuery = window.jQuery;
+
+    var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
+        itemSelector: '.shuffle-item',
+        buffer: 1
+    });
+
+    jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
+        var input = evt.currentTarget;
+        if (input.checked) {
+            myShuffle.filter(input.value);
+        }
+    });
+
+
+
+})(jQuery);
