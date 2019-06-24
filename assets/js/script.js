daf5128c03a5e09523208cc65e596f9c9bb171b8
somrat <monnaf37@gmail.com>
2019-06-24 09:32:31 +0600

    modified responsive issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -142,20 +142,21 @@
     });
 
     // Shuffle js filter and masonry
-    var Shuffle = window.Shuffle;
-    var jQuery = window.jQuery;
+    var containerEl = document.querySelector('.shuffle-wrapper');
+    if (containerEl) {
+        var Shuffle = window.Shuffle;
+        var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
+            itemSelector: '.shuffle-item',
+            buffer: 1
+        });
 
-    var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
-        itemSelector: '.shuffle-item',
-        buffer: 1
-    });
-
-    jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
-        var input = evt.currentTarget;
-        if (input.checked) {
-            myShuffle.filter(input.value);
-        }
-    });
+        jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
+            var input = evt.currentTarget;
+            if (input.checked) {
+                myShuffle.filter(input.value);
+            }
+        });
+    }
 
 
 
