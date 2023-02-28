20a80085009bfbed9daae0b4e40639bdc03fb818
tfsumon <sumon.themefisher@gmail.com>
2023-02-28 11:27:19 +0600

    configured shuffle js for project filtering

--- assets/js/script.js
+++ assets/js/script.js
@@ -137,21 +137,21 @@
 		},
 	});
 
-	// shuffle js
-	let filterContainer = document.querySelector('.filter-container');
-	if (isElementExist(filterContainer)) {
-		new Filterizr('.filter-container')
-	}
-
-	// active changer
-	let control = document.querySelectorAll('.control');
-	control.forEach(function (item) {
-		item.addEventListener('click', function () {
-			control.forEach(function (item) {
-				item.classList.remove('active');
-			});
-			this.classList.add('active');
+	// Shuffle js filter and masonry
+	var containerEl = document.querySelector('.shuffle-wrapper');
+	if (containerEl) {
+		var Shuffle = window.Shuffle;
+		var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
+			itemSelector: '.shuffle-item',
+			buffer: 1
 		});
-	});
+
+		jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
+			var input = evt.currentTarget;
+			if (input.checked) {
+				myShuffle.filter(input.value);
+			}
+		});
+	}
 
 })(jQuery);
